1d9ad72386d5f9c8e830be5e96a3c5411a013343
Brandon Trump <brandontrump101@gmail.com>
2023-06-15 21:36:13 +1200

    style: adjusted form labels

--- components/styles/CalculatorCard.styled.ts
+++ components/styles/CalculatorCard.styled.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const StyledCard = styled.div`
   background-color: white;
-  padding: 2em;
+  padding: 3em 4em;
   border-radius: 1.5em;
   border-bottom-right-radius: 7em;
 
   form {
     display: flex;
     flex-direction: column;
-    gap: 1em;
+    gap: 2em;
     margin-bottom: 2em;
 
     button {
@@ -31,18 +31,21 @@ export const StyledCard = styled.div`
 export const StyledInput = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 0.5em;
 
   label {
     text-transform: uppercase;
     font-size: 0.8em;
+    font-weight: 600;
+    color: rgb(130 130 130);
   }
 
   input {
     max-width: 4em;
     padding: 0.5em;
     border-radius: 0.3em;
-    border-width: 0.1em;
-    border-color: lightgray;
+    border-width: 0.05em;
+    border-color: rgb(130 130 130);
   }
 `;
 
@@ -50,6 +53,9 @@ export const StyledDate = styled.div`
   h1 {
     margin: 0;
     padding: 0;
+    font-style: italic;
+    font-weight: 900;
+    font-size: 2.8em;
 
     span {
       color: rgb(134 76 255);
